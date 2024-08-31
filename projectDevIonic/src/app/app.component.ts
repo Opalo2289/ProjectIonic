b5790d7f627147b11942e8bf058a671994dd2f8e
israel1947 <israeldavidsdvsf@gmail.com>
2024-08-31 15:16:05 +0200

    refactor(app component): se crea menu component
    se crea manu componet para manejar el  side menu
    se implementa el menu component en el app compoenent
    se divide el menu por secciones "persona" y "compras"
    se arregla error de menú al hacer clic en el submenu

--- projectDevIonic/src/app/app.component.ts
+++ projectDevIonic/src/app/app.component.ts
@@ -1,39 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonAccordion, IonAccordionGroup } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { IonApp, IonSplitPane, IonRouterOutlet, IonRouterLink, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { SpecialtyPage } from "./specialty/specialty.page";
+import { MenuComponent } from './components/menu/menu.component';
+import { IonAvatar } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonAccordionGroup, IonAccordion, RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet, SpecialtyPage],
+  imports: [IonToolbar, IonTitle,  IonApp, IonSplitPane, IonRouterLink, IonRouterOutlet, SpecialtyPage, MenuComponent, IonAvatar],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Citas y Examenes', url: '/folder/busqueda_citas', icon: 'mail' },
-    { title: 'Agendamiento de citas', url: '/specialty/Gestion de citas medicas', icon: 'mail' },
-    { 
-      title: 'Usuarios', 
-      url: '/folder/usuarios', 
-      icon: 'mail',
-      subPages: [
-        { title: 'Permisos de usurarios', url: '/folder/listar_usuarios', icon: 'list' },
-        { title: 'Gestionar usuario', url: '/folder/crear_usuario', icon: 'person-add' },
-      ]
-    },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
-  }
+  idValue: string = 'main-content';
 }
---
+++ projectDevIonic/src/app/components/menu/menu.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonAccordion, IonAccordionGroup, IonHeader, IonToolbar, IonTitle, IonTabs, IonTabButton, IonTabBar, IonSearchbar, IonMenuButton, IonButtons, IonAvatar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, calendarOutline, peopleOutline, calendarNumberOutline, keyOutline, personAddOutline, cardOutline, storefrontOutline } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+  standalone: true,
+  imports: [IonAvatar,
+    IonSearchbar, IonTabBar, IonTabButton, IonTabs, IonTitle, IonToolbar, IonHeader, IonAccordionGroup, IonAccordion, RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet, IonMenuButton, IonButtons
+  ],
+})
+export class MenuComponent {
+  @Input() idValue!: string;
+
+
+  public appPages = {
+    personal: [
+      { title: 'Citas y Examenes', url: '/folder/busqueda_citas', icon: 'calendar-number' },
+      { title: 'Agendamiento de citas', url: '/specialty/Gestion de citas medicas', icon: 'calendar' },
+      {
+        title: 'Usuarios',
+        url: '/folder/usuarios',
+        icon: 'people',
+        subPages: [
+          { title: 'Permisos de usuarios', url: '/folder/listar_usuarios', icon: 'key' },
+          { title: 'Gestionar usuario', url: '/folder/crear_usuario', icon: 'person-add' }
+        ]
+      },
+      { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
+      { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+      { title: 'Archived', url: '/folder/archived', icon: 'archive' },
+      { title: 'Trash', url: '/folder/trash', icon: 'trash' },
+      { title: 'Spam', url: '/folder/spam', icon: 'warning' }
+    ],
+    comparas: [
+      { title: 'Pedidos y Pagos', url: '/folder/outbox', icon: 'card' },
+      { title: 'Marketplace', url: '/marketplace', icon: 'storefront' }
+    ]
+  };
+
+  public labels = [
+    { title: 'Pedidos y Pagos', url: '/folder/outbox', icon: 'card' },
+    { title: 'Marketplace', url: '/folder/favorites', icon: 'storefront' },
+  ];
+
+  constructor() {
+    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, calendarOutline, peopleOutline, calendarNumberOutline, keyOutline, personAddOutline, cardOutline, storefrontOutline });
+  }
+
+}
